bbe5175a9cfa91347dcba39daec39638e798ae6f
Mahboob Karimian <63875592+mahboobkarimian@users.noreply.github.com>
2021-11-14 14:26:55 +0100

    remove sudo for files going to user home dir

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ echo "#### Copying files ####"
 sudo cp -v AppleT2.conf /usr/share/alsa/cards/AppleT2.conf
 sudo cp -v apple-t2.conf /usr/share/pulseaudio/alsa-mixer/profile-sets/apple-t2.conf
 sudo cp -v 91-pulseaudio-custom.rules /usr/lib/udev/rules.d/91-pulseaudio-custom.rules
-sudo cp -v default.pa $HOME/.config/pulse/default.pa
-sudo cp -v daemon.conf $HOME/.config/pulse/daemon.conf
+cp -v default.pa $HOME/.config/pulse/default.pa
+cp -v daemon.conf $HOME/.config/pulse/daemon.conf
 echo "#### Restarting pulseaudio ####"
 systemctl --user restart pulseaudio.service
 echo "#### Increasing mic level by 400% ####"
