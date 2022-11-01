779f82293af0f84dcbecfb76dac0b51c42f7f7ab
Mahboob <mahboob.karimiyan@gmail.com>
2022-11-01 13:58:16 +0100

    Added capability to newer apple-bce audio driver

--- poll_mic.sh
+++ poll_mic.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-index=`pacmd list-sources | grep builtin-mic -B 2 | grep index | cut -d ":" -f 2 | tr -d " "`
+if $(grep -w -q AppleT2 /proc/asound/cards);
+then
+	index=`pacmd list-sources | grep builtin-mic -B 2 | grep index | cut -d ":" -f 2 | tr -d " "`
+else
+	index=`pacmd list-sources | grep BuiltinMic -B 2 | grep index | cut -d ":" -f 2 | tr -d " "`
+fi
 pactl subscribe | grep --line-buffered "source #$index" | xargs -n1 ~/mbp-2019-Ubuntu-audio/volume_notify.sh
--- volume_notify.sh
+++ volume_notify.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-index=`pacmd list-sources | grep builtin-mic -B 2 | grep index | cut -d ":" -f 2 | tr -d " "`
+if $(grep -w -q AppleT2 /proc/asound/cards);
+then
+	index=`pacmd list-sources | grep builtin-mic -B 2 | grep index | cut -d ":" -f 2 | tr -d " "`
+else
+	index=`pacmd list-sources | grep BuiltinMic -B 2 | grep index | cut -d ":" -f 2 | tr -d " "`
+fi
 pacmd set-source-volume $index 0x40000
